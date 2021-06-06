60ec90dc2926d239af8f261a86caa040dcc65e6c
Sarthak Naithani <sarthaknaithani@yahoo.in>
2021-06-06 17:50:08 +0530

    Create InningsMain.java

---
+++ Activity2/InningsMain.java
@@ -0,0 +1,65 @@
+mport java.util.Scanner;
+
+class Innings
+{
+	private String teamname;
+	private String inningsname;
+	private int runs;
+	
+	void setteamname(String teamname)
+	{
+		this.teamname = teamname;
+	}
+	void setinningsname(String inningsname)
+	{
+		this.inningsname= inningsname;
+	}
+	void setruns(int runs)
+	{
+		this.runs = runs;
+	}
+  String getteamname() {
+		return this.teamname;
+	}
+	String getinningsname() {
+		return this.inningsname;
+	}
+  int getruns()
+  {
+	  return this.runs;
+  }
+  			
+  			void displayInningsDetails()
+  			{  
+  				System.out.println("Employee Details");
+  				System.out.println("Name:" + getteamname());
+  				System.out.println("Scored:" + getruns());
+  				if(getinningsname().equals("first"))
+  				{
+  					System.out.println("Need "+ (runs+1) + "to win");
+  				}
+  				else
+  				{
+  					System.out.println("Match Ended");
+  				}
+  			}
+}
+
+public class InningsMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Innings E = new Innings();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the team name:");
+		E.setteamname(sc.nextLine());
+		System.out.println("Enter Session:");
+		E.setinningsname(sc.nextLine());
+		System.out.println("Enter runs:");
+		E.setruns(sc.nextInt());
+		 
+		E.displayInningsDetails();
+	}
+
+}
